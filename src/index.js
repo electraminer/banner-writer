28d2528951c9a08d4511caccf9415d4592e72fe5
Electra <arthurlschell@gmail.com>
2023-11-18 04:13:05 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+import './index.css';
+
+import App from './components/App.js';
+import { BannerContextProvider } from 'components/context/BannerContext';
+import { WritingContextProvider } from 'components/context/WritingContext';
+import { RecentContextProvider } from 'components/context/RecentContext';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { SavedContextProvider } from 'components/context/SavedContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SavedContextProvider>
+      <RecentContextProvider>
+        <BannerContextProvider>
+          <WritingContextProvider>
+            <App />
+          </WritingContextProvider>
+        </BannerContextProvider>
+      </RecentContextProvider>
+    </SavedContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
