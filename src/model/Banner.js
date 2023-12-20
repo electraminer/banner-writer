21c614264f65a132df1bde2402febe52a9b6c1a9
Electra <arthurlschell@gmail.com>
2023-12-20 17:21:19 -0500

    Added banner code support

--- src/model/Banner.js
+++ src/model/Banner.js
@@ -58,4 +58,33 @@ export default class Banner {
     static fromCharacters(characters) {
         return Writing.fromCharacters(characters)[0];
     }
+
+    static fromBannerCode(code) {
+        console.log(code);
+        const patternCodes = `b
+            bo|bri|mc|cre|cr|cbo|ld|rud|lud|rd
+            flo|glb|gra|gru|hh|hhb|vh|vhr|moj|pig
+            mr|sku|ss|bl|br|tl|tr|sc|bs|cs
+            dls|drs|ls|ms|rs|ts|bt|tt|bts|tts
+        `.trim().split(/\||\n/).map(s=>s.trim());
+        const colorCodes = `
+            0|8|7|15|4|1|14|12
+            5|13|3|9|11|6|2|10
+        `.trim().split(/\||\n/).map(s=>s.trim());
+
+        let background = undefined;
+        let layers = [];
+        for (const [s, p, c] of [...code.matchAll(/([a-z]+)([0-9]+)/g)]) {
+            let pattern = patternCodes.indexOf(p);
+            let color = colorCodes.indexOf(c);
+
+            if (pattern == 0) {
+                background = color;
+            } else {
+                layers.push(new Layer(color, pattern));
+            }
+        }
+
+        return new Banner(background, ...layers);
+    }
 }
